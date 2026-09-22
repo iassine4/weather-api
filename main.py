d9d960806e86df2f9d5a0a9dfd9cf980153f869d
IAssine <ia.ssine3325@gmail.com>
2026-09-22 15:57:47 +0200

    deplacer le fichier main.py dans la racine du projet

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 forecast_url = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = os.getenv("OPENWEATHER_API_KEY")
 
+print("Clé chargée :", api_key is not None)
+print("Longueur de la clé :", len(api_key) if api_key else 0)
+
 city = "Mérignac,FR"
 
 params = {
